602e77fa4b6e394bf6703a6fbeccad4631b608e9
rbahena <ramiro.bah11@gmail.com>
2023-09-18 23:10:52 -0600

    Agregar entidad usuarios

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, PrimaryColumn } from 'typeorm';
+
+@Entity({ name: 'usuarios' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id_usuario: number;
+
+  @Column({ unique: true })
+  correo_electronico: string;
+
+  @Column()
+  contrasena: string;
+
+  @Column({ type: 'datetime', default: () => 1 })
+  periodo_prueba: boolean;
+
+  @Column({ type: 'datetime', default: () => 'current_timestamp' })
+  inicio_prueba: Date;
+
+  @Column()
+  fin_prueba: Date;
+
+  @Column({ nullable: true, default: 'null' })
+  auth_strategy: string;
+
+  @Column()
+  estatus: boolean;
+
+  @Column({ type: 'datetime', default: () => 'current_timestamp' })
+  fecha_alta: Date;
+
+  @Column({ type: 'datetime', nullable: true, default: 'null' })
+  fecha_baja: Date;
+}
