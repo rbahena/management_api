1b660968a499bc08d2754a8f5165d06b4ab60112
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-09-19 15:56:45 -0600

    Agregar documentacion con swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Add swagger
+  const config = new DocumentBuilder()
+    .setTitle('Management API')
+    .setDescription('My description')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('Documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
