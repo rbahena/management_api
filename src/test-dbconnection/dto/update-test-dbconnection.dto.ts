cb1e14a14a7f762c667b1809e8f0b33cb7fa7ab0
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-09-26 10:34:15 -0600

    Borrar recurso test

--- src/test-dbconnection/dto/update-test-dbconnection.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTestDbconnectionDto } from './create-test-dbconnection.dto';
-
-export class UpdateTestDbconnectionDto extends PartialType(CreateTestDbconnectionDto) {}
--- src/test-dbconnection/test-dbconnection.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestDbconnectionService } from './test-dbconnection.service';
-import { TestDbconnectionController } from './test-dbconnection.controller';
-
-@Module({
-  controllers: [TestDbconnectionController],
-  providers: [TestDbconnectionService],
-})
-export class TestDbconnectionModule {}
