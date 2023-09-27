5f624c9a24063e832560f9eef2459a8e1ebbee2c
rbahena <ramiro.bah11@gmail.com>
2023-09-26 22:07:46 -0600

    Agregar encontrar un suscriptor

--- src/suscriptores/suscriptores.service.ts
+++ src/suscriptores/suscriptores.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSuscriptoreDto } from './dto/create-suscriptore.dto';
 import { UpdateSuscriptoreDto } from './dto/update-suscriptore.dto';
 import { Suscriptor } from './entities/suscriptor.entity';
@@ -8,11 +8,15 @@ import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 @Injectable()
 export class SuscriptoresService {
-  constructor(@InjectRepository(Suscriptor) private suscriptorRepository: Repository<Suscriptor>) {}
+  constructor(
+    @InjectRepository(Suscriptor)
+    private suscriptorRepository: Repository<Suscriptor>,
+  ) {}
 
   async create(createSuscriptoreDto: CreateSuscriptoreDto) {
     try {
-      const newSuscriptor = this.suscriptorRepository.create(createSuscriptoreDto);
+      const newSuscriptor =
+        this.suscriptorRepository.create(createSuscriptoreDto);
       return this.suscriptorRepository.save(newSuscriptor);
     } catch (error) {
       throw new ExceptionsHandler(error);
@@ -20,15 +24,32 @@ export class SuscriptoresService {
   }
 
   findAll() {
-    return this.suscriptorRepository.find({relations:['fk_usuario']});
+    return this.suscriptorRepository.find({ relations: ['fk_usuario'] });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} suscriptore`;
+  async findOne(id: number) {
+    const suscriptor = await this.suscriptorRepository.findOne({
+      where: { id_suscriptor: id },
+      relations: ['fk_usuario'],
+    });
+    if (!suscriptor)
+      throw new HttpException(
+        'No se encontró la información solicitada',
+        HttpStatus.NOT_FOUND,
+      );
+    return this.suscriptorRepository.findOne({
+      where: { id_suscriptor: id },
+      relations: ['fk_usuario'],
+    });
   }
 
-  update(id: number, updateSuscriptoreDto: UpdateSuscriptoreDto) {
-    return `This action updates a #${id} suscriptore`;
+  async update(id: number, updateSuscriptoreDto: UpdateSuscriptoreDto) {
+    const suscriptor = await this.suscriptorRepository.findOne({
+      where: { id_suscriptor: id, estatus: 1 },
+    });
+    console.log(suscriptor);
+    if (!suscriptor) throw new HttpException('El usuario no existe', HttpStatus.NOT_FOUND);;
+    return this.suscriptorRepository.update({ id_suscriptor: id }, updateSuscriptoreDto);
   }
 
   remove(id: number) {
