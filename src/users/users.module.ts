da445cee99f61d6c3f6792d6c90f21f7733f9f98
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-02 17:26:28 -0600

    Configuracion del JWT

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,16 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Suscriptor } from 'src/suscriptores/entities/suscriptor.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User, Suscriptor])],
+  imports: [
+    TypeOrmModule.forFeature([User, Suscriptor]),
+    JwtModule.register({
+      secret: 'SECRETO',
+      signOptions: { expiresIn: '28800s' },
+    }),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
