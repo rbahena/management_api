bf79e40ced1926110552222e74e6d4e48ea6f8b5
rbahena <ramiro.bah11@gmail.com>
2023-10-11 23:06:31 -0600

    Agregar modulos al app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InventarioModule } from './modules/inventario/inventario.module';
+import { ProveedorModule } from './modules/proveedor/proveedor.module';
+import { ProductoModule } from './modules/producto/producto.module';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { PreciosModule } from './modules/precios/precios.module';
 
 @Module({
-  imports: [],
+  imports: [
+    InventarioModule,
+    PreciosModule,
+    ProductoModule,
+    ProveedorModule,
+    UsuarioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
