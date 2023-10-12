1badf3677711d9f5cfde628def7528ccfb255fc9
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-12 16:05:11 -0600

    Agregar detalle de dtos para swagger

--- src/modules/usuarios/autenticacion/dtos/registroUsuario.dto.ts
+++ src/modules/usuarios/autenticacion/dtos/registroUsuario.dto.ts
@@ -1,1 +1,15 @@
-export class registroUsuario {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class registroUsuario {
+  @ApiProperty()
+  correo_electronico: string;
+
+  @ApiProperty()
+  contrasena: string;
+
+  @ApiProperty()
+  nombre_operacion_prueba: string;
+
+  @ApiProperty()
+  auth_strategy: string;
+}
