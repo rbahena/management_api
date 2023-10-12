451270f6a21cae267c3bf6c4dd9e788f3c6b2e81
rbahena <ramiro.bah11@gmail.com>
2023-10-11 23:54:19 -0600

    Agregar servicio de autenticacion

--- src/modules/usuarios/autenticacion/autenticacion.controller.ts
+++ src/modules/usuarios/autenticacion/autenticacion.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AutenticacionService } from './autenticacion.service';
 
 @Controller('autenticacion')
-export class AutenticacionController {}
+export class AutenticacionController {
+  constructor(private readonly autenticacionService: AutenticacionService) {}
+
+//   @Post('createUser')
+//   create(@Body() createUserDto: CreateUserDto): Promise<User> {
+//     return this.usersService.create(createUserDto);
+//   }
+}
