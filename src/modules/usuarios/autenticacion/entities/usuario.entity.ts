a440c98d4f1f33320b638d6400bee2288458aa4c
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-12 16:05:31 -0600

    Eliminar controlador principal y typeorm error

--- src/modules/usuarios/autenticacion/entities/usuario.entity.ts
+++ src/modules/usuarios/autenticacion/entities/usuario.entity.ts
@@ -1,41 +1,40 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
 
-@Entity({ name: 'usuarios' })
+// @Entity({ name: 'usuarios' })
 export class User {
-  @PrimaryGeneratedColumn()
-  id_usuario: number;
+  // @PrimaryGeneratedColumn()
+  // id_usuario: number;
 
-  @Column({ type: 'varchar', unique: true })
-  correo_electronico: string;
+  // @Column({ type: 'varchar', unique: true })
+  // correo_electronico: string;
 
-  @Column({ type: 'text' })
-  contrasena: string;
+  // @Column({ type: 'text' })
+  // contrasena: string;
 
-  @Column({ type: 'int', default: 0 })
-  correo_confirmado: number;
+  // @Column({ type: 'int', default: 0 })
+  // correo_confirmado: number;
 
-  @Column({ type: 'varchar' })
-  nombre_operacion_prueba: string;
+  // @Column({ type: 'varchar' })
+  // nombre_operacion_prueba: string;
 
-  @Column({ type: 'int', default: 1 })
-  periodo_prueba: number;
+  // @Column({ type: 'int', default: 1 })
+  // periodo_prueba: number;
 
-  @Column({ type: 'text', nullable: true, default: 'null' })
-  auth_strategy: string;
+  // @Column({ type: 'text', nullable: true, default: 'null' })
+  // auth_strategy: string;
 
-  @Column({ type: 'timestamp', default: () => 'NOW()' })
-  inicio_prueba: Date;
+  // @Column({ type: 'timestamp', default: () => 'NOW()' })
+  // inicio_prueba: Date;
 
-  @Column({ type: 'timestamp', default: () => 'NOW() + interval 1 day' })
-  fin_prueba: Date;
+  // @Column({ type: 'timestamp', default: () => 'NOW() + interval 1 day' })
+  // fin_prueba: Date;
 
-  @Column({ type: 'int', default: 1 })
-  estatus: number;
+  // @Column({ type: 'int', default: 1 })
+  // estatus: number;
 
-  @Column({ type: 'timestamp', default: () => 'NOW()' })
-  fecha_alta: Date;
+  // @Column({ type: 'timestamp', default: () => 'NOW()' })
+  // fecha_alta: Date;
 
-  @Column({ type: 'timestamp', nullable: true })
-  fecha_baja: Date;
+  // @Column({ type: 'timestamp', nullable: true })
+  // fecha_baja: Date;
   
 }
