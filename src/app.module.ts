cc0aa4cffcea511b11826eb8b0d7fa899a887eb3
rbahena <ramiro.bah11@gmail.com>
2023-10-11 23:15:10 -0600

    Actualizar nombre de modulos principales

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InventarioModule } from './modules/inventario/inventario.module';
-import { ProveedorModule } from './modules/proveedor/proveedor.module';
-import { ProductoModule } from './modules/producto/producto.module';
-import { UsuarioModule } from './modules/usuario/usuario.module';
+import { ProveedoresModule } from './modules/proveedores/proveedores.module';
+import { ProductosModule } from './modules/productos/productos.module';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
 import { PreciosModule } from './modules/precios/precios.module';
 
 @Module({
   imports: [
     InventarioModule,
     PreciosModule,
-    ProductoModule,
-    ProveedorModule,
-    UsuarioModule,
+    ProductosModule,
+    ProveedoresModule,
+    UsuariosModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/usuarios/usuarios.module.ts
+++ src/modules/usuarios/usuarios.module.ts
@@ -1,4 +1,4 @@
 import { Module } from '@nestjs/common';
 
 @Module({})
-export class ProductoModule {}
+export class UsuariosModule {}
