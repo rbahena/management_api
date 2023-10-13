794deb55470075faa5983c9d13c89413bec41cc9
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-13 14:00:33 -0600

    Instalar paquete para manejo de ambientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { InventarioModule } from './modules/inventario/inventario.module';
 import { ProveedoresModule } from './modules/proveedores/proveedores.module';
 import { ProductosModule } from './modules/productos/productos.module';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
 import { PreciosModule } from './modules/precios/precios.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'environments/develop.env',
+      isGlobal: true,
+    }),
     InventarioModule,
     PreciosModule,
     ProductosModule,
--- src/modules/usuarios/autenticacion/autenticacion.controller.ts
+++ src/modules/usuarios/autenticacion/autenticacion.controller.ts
@@ -3,21 +3,25 @@ import { AutenticacionService } from './autenticacion.service';
 import { registroUsuarioDto } from './dtos/registroUsuario.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { iniciaSesionDto } from './dtos/iniciaSesion.dto';
+import { ConfigService } from '@nestjs/config';
 
 @ApiTags('Autenticación')
 @Controller('autenticacion')
 export class AutenticacionController {
-  constructor(private readonly autenticacionService: AutenticacionService) {}
+  constructor(
+    private readonly autenticacionService: AutenticacionService,
+    private readonly configService: ConfigService,
+  ) {}
 
-  @Post("registrarUsuario")
+  @Post('registrarUsuario')
   registrarUsuario(@Body() registroUsuarioDto: registroUsuarioDto) {
+    const api_key = this.configService.get('API_KEY');
+    console.log(api_key);
     return this.autenticacionService.registrarUsuario(registroUsuarioDto);
   }
 
-  @Post("iniciarSesion")
+  @Post('iniciarSesion')
   iniciarSesion(@Body() iniciaSesionDto: iniciaSesionDto) {
     return this.autenticacionService.inciaSesion(iniciaSesionDto);
   }
-
-
 }
