91a4e30418659e5d7d42efd10e50f8de3a32c10d
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-17 17:48:47 -0600

    Agregar entidad usuario y definicion en modulo

--- src/modules/usuarios/autenticacion/entities/usuario.entity.ts
+++ src/modules/usuarios/autenticacion/entities/usuario.entity.ts
@@ -1,40 +1,53 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-// @Entity({ name: 'usuarios' })
+ @Entity({ name: 'usuarios' })
 export class User {
-  // @PrimaryGeneratedColumn()
-  // id_usuario: number;
+  //@PrimaryGeneratedColumn()
+  @Column()
+  id_usuario: number;
 
-  // @Column({ type: 'varchar', unique: true })
-  // correo_electronico: string;
+  //@Column({ type: 'varchar', unique: true })
+  @Column()
+  correo_electronico: string;
 
-  // @Column({ type: 'text' })
-  // contrasena: string;
+  //@Column({ type: 'text' })
+  @Column()
+  contrasena: string;
 
-  // @Column({ type: 'int', default: 0 })
-  // correo_confirmado: number;
+  //@Column({ type: 'int', default: 0 })
+  @Column()
+  correo_confirmado: number;
 
-  // @Column({ type: 'varchar' })
-  // nombre_operacion_prueba: string;
+  //@Column({ type: 'varchar' })
+  @Column()
+  nombre_operacion_prueba: string;
 
-  // @Column({ type: 'int', default: 1 })
-  // periodo_prueba: number;
+  //@Column({ type: 'int', default: 1 })
+  @Column()
+  periodo_prueba: number;
 
-  // @Column({ type: 'text', nullable: true, default: 'null' })
-  // auth_strategy: string;
+  //@Column({ type: 'text', nullable: true, default: 'null' })
+  @Column()
+  auth_strategy: string;
 
-  // @Column({ type: 'timestamp', default: () => 'NOW()' })
-  // inicio_prueba: Date;
+  //@Column({ type: 'timestamp', default: () => 'NOW()' })
+  @Column()
+  inicio_prueba: Date;
 
-  // @Column({ type: 'timestamp', default: () => 'NOW() + interval 1 day' })
-  // fin_prueba: Date;
+  //@Column({ type: 'timestamp', default: () => 'NOW() + interval 1 day' })
+  @Column()
+  fin_prueba: Date;
 
-  // @Column({ type: 'int', default: 1 })
-  // estatus: number;
+  //@Column({ type: 'int', default: 1 })
+  @Column()
+  estatus: number;
 
-  // @Column({ type: 'timestamp', default: () => 'NOW()' })
-  // fecha_alta: Date;
+  //@Column({ type: 'timestamp', default: () => 'NOW()' })
+  @Column()
+  fecha_alta: Date;
 
-  // @Column({ type: 'timestamp', nullable: true })
-  // fecha_baja: Date;
+  //@Column({ type: 'timestamp', nullable: true })
+  @Column()
+  fecha_baja: Date;
   
 }
--- src/modules/usuarios/usuarios.module.ts
+++ src/modules/usuarios/usuarios.module.ts
@@ -1,16 +1,36 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AutenticacionController } from './autenticacion/autenticacion.controller';
 import { AutenticacionService } from './autenticacion/autenticacion.service';
+import { User } from './autenticacion/entities/usuario.entity';
+
 import { SuscriptoresController } from './suscriptores/suscriptores.controller';
 import { SuscriptoresService } from './suscriptores/suscriptores.service';
+
 import { PlanesPagoController } from './planes-pago/planes-pago.controller';
 import { PlanesPagoService } from './planes-pago/planes-pago.service';
+
 import { OperacionesController } from './operaciones/operaciones.controller';
 import { OperacionesService } from './operaciones/operaciones.service';
 
+
+
+
 @Module({
-  controllers: [AutenticacionController, SuscriptoresController, PlanesPagoController, OperacionesController],
-  providers: [AutenticacionService, SuscriptoresService, PlanesPagoService, OperacionesService],
-  exports: []
+  imports:[TypeOrmModule.forFeature([User])],
+  controllers: [
+    AutenticacionController,
+    SuscriptoresController,
+    PlanesPagoController,
+    OperacionesController,
+  ],
+  providers: [
+    AutenticacionService,
+    SuscriptoresService,
+    PlanesPagoService,
+    OperacionesService,
+  ],
+  exports: [],
 })
 export class UsuariosModule {}
