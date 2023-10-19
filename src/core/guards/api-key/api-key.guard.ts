0045649ec8021a8d86d01bd9d79b59935b96df90
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-18 18:29:39 -0600

    Agregar opcion de dejar publico algunos endpoint

--- src/core/guards/api-key/api-key.guard.ts
+++ src/core/guards/api-key/api-key.guard.ts
@@ -1,19 +1,24 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get('isPublic', context.getHandler());
+    if (isPublic) return true;
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Auth');
-    const isAuth = authHeader === '123'
-    if(!isAuth) {
-      throw new UnauthorizedException('No tienes permisos para acceder.'); 
-    }
+    const isAuth = authHeader === '123';
+    if (!isAuth) throw new UnauthorizedException('No tienes permisos para acceder.');
     return isAuth;
-
   }
 }
--- src/modules/usuarios/autenticacion/autenticacion.controller.ts
+++ src/modules/usuarios/autenticacion/autenticacion.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Inject, Post, SetMetadata, UseGuards } from '@nestjs/common';
 import { ApiKeyGuard } from 'src/core/guards/api-key/api-key.guard';
 import { registroUsuarioDto } from './dtos/registroUsuario.dto';
 import { AutenticacionService } from './autenticacion.service';
@@ -17,12 +17,15 @@ export class AutenticacionController {
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
 
+  @SetMetadata('isPublic', true)
   @Post('registrarUsuario')
   registrarUsuario(
     @Body() registroUsuarioDto: registroUsuarioDto,
   ): Promise<User> {
     return this.autenticacionService.registrarUsuario(registroUsuarioDto);
   }
+
+  
 
   @Post('iniciarSesion')
   iniciarSesion(@Body() iniciaSesionDto: iniciaSesionDto): Promise<User> {
